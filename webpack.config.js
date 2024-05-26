02247b9fea9a6b4dbca24ea9130851d8f173af12
Maxim Lesev <lesov0iblr@gmail.com>
2024-05-26 16:39:00 +0200

    Add to opportunity to use 2 modes for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
+module.exports = (env) => {
+    return {
+        mode: env.mode ?? 'development',    // This code allows to use to scripts for dev and prod development
+        entry: './src/index.js',
+        output: {
+            filename: '[name].[contenthash].js',    // This line allows webpack to create other name for js file. When the browser will check this file, if we will not add changes, the browser will take data from cache.
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+    }
 };

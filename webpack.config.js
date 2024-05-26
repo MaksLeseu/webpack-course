c9c192eb60e3f47f5b572c4e4f3002f2eb55078e
Maxim Lesev <lesov0iblr@gmail.com>
2024-05-26 16:58:06 +0200

    Create index.html file and add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
     return {
-        mode: env.mode ?? 'development',    // This code allows to use to scripts for dev and prod development
-        entry: './src/index.js',
+        mode: env.mode ?? 'development',            // This code allows to use to scripts for dev and prod development
+        entry: path.resolve(__dirname, 'src', 'index.js'),
         output: {
             filename: '[name].[contenthash].js',    // This line allows webpack to create other name for js file. When the browser will check this file, if we will not add changes, the browser will take data from cache.
             path: path.resolve(__dirname, 'dist'),
-            clean: true
+            clean: true                             // to remove main.js when the webpack is working agan
         },
+        plugins: [
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+        ],
     }
 };

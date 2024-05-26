8e6a5e13b5da5650d4f89420b01e0b381ea7c9dc
Maxim Lesev <lesov0iblr@gmail.com>
2024-05-26 17:18:23 +0200

    Create a loader for typescript and add types for typescript, node, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = (env) => {
     return {
         mode: env.mode ?? 'development',            // This code allows to use to scripts for dev and prod development
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
         output: {
             filename: '[name].[contenthash].js',    // This line allows webpack to create other name for js file. When the browser will check this file, if we will not add changes, the browser will take data from cache.
             path: path.resolve(__dirname, 'dist'),
             clean: true                             // to remove main.js when the webpack is working agan
         },
         plugins: [
-            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })
+            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })  // This is a plugin to allow me to insert js file to the index.html file
         ],
     }
 };
